efdc0dbacea674cc994bb983eae2b86dcca8cb9e
Edson Primo <edson.oliveira00@hotmail.com>
2021-04-28 16:04:15 -0300

    media query 1.1

---
+++ src/static/js/form.js
@@ -0,0 +1,9 @@
+let btnEnviar = document.querySelector('#enviar')
+
+btnEnviar.addEventListener('click', function(){
+    informacoesForm = document.querySelectorAll('input')
+    
+    for (let i=0; i < informacoesForm.length; i++) {
+            let nome = informacoesForm[0]
+    }
+})
