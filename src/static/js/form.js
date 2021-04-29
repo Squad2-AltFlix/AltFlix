f9cd06eee62509b6d22d7efb1c16af5ce652045a
Edson Primo <edson.oliveira00@hotmail.com>
2021-04-29 12:06:40 -0300

    Form com Header e Validações

--- src/static/js/form.js
+++ src/static/js/form.js
@@ -1,11 +1,8 @@
 /* função para aparecer um input quando selecionado outro no select */
-let static = 0
 function doSomething() {
    let divOculta = document.querySelector(".div-oculta")
    divOculta.classList.remove('div-oculta')
    divOculta.classList.add('div-no-oculta')
-
-   static ++
 
 }
 
@@ -41,13 +38,10 @@ btnEnviar.addEventListener("click", function enviar() {
       sinopse: informacoes[9]
    }
 
-
    let escolhaGenero = document.querySelector("#genero").value
    if (escolhaGenero.length > 0) {
       informacoesForm.genero = escolhaGenero
    }
-   console.log()
-
 
    /***************** VALIDAÇÂO ************************/
 
@@ -55,57 +49,60 @@ btnEnviar.addEventListener("click", function enviar() {
    validacao(informacaoFilme)
 
    function validacao(form) {
-      
+
       /*nome validação*/
       if (form.nome.length == 0) {
-         document.querySelector("#nome").classList.add("small")
+         document.querySelector("#nome").classList.add("div-small")
          error.push('erro')
       }
-      else document.querySelector("#nome").classList.remove("small")
-         
+      else document.querySelector("#nome").classList.remove("div-small")
+
       /*email validação*/
       let emailSearch = form.email.search("@")
       if (emailSearch == -1 || form.email.length < 7) {
-         document.querySelector("#email").classList.add("small")
+         document.querySelector("#email").classList.add("div-small")
+         form.email = ''
          error.push('erro')
       }
-      else document.querySelector("#email").classList.remove("small")
+      else document.querySelector("#email").classList.remove("div-small")
+
 
       /* validação email */
       const isValidPhone = (phone) => {
-         const sanitizedPhone = phone.replace(/\D/g,'');
+         const sanitizedPhone = phone.replace(/\D/g, '');
          return sanitizedPhone.length >= 10 && sanitizedPhone.length <= 11;
-       };
+      };
       if (!isValidPhone(form.tel)) {
-         document.querySelector("#tel").classList.add("small")
+         document.querySelector("#tel").classList.add("div-small")
          error.push('erro')
-      } else document.querySelector("#tel").classList.remove("small")
+      } else document.querySelector("#tel").classList.remove("div-small")
+
 
       /* validação select */
       if (form.genero == '' || form.genero == 'Selecione') {
-         document.querySelector("#selectOculto").classList.add("small")
+         document.querySelector("#selectOculto").classList.add("div-small")
          error.push('erro')
-      } else document.querySelector("#selectOculto").classList.remove("small")
+      } else document.querySelector("#selectOculto").classList.remove("div-small")
 
       /*validação titulo*/
       if (form.titulo.length == 0) {
-         document.querySelector("#titulo-oculto").classList.add("small")
+         document.querySelector("#titulo-oculto").classList.add("div-small")
          error.push('erro')
       }
-      else document.querySelector("#titulo-oculto").classList.remove("small")
+      else document.querySelector("#titulo-oculto").classList.remove("div-small")
 
-       /*validação data de lançamento*/
-       if (form.dataLancamento.length == 0) {
-         document.querySelector("#data-oculto").classList.add("small")
+      /*validação data de lançamento*/
+      if (form.dataLancamento.length == 0) {
+         document.querySelector("#data-oculto").classList.add("div-small")
          error.push('erro')
       }
-      else document.querySelector("#data-oculto").classList.remove("small")
+      else document.querySelector("#data-oculto").classList.remove("div-small")
 
    }
 
    if (error.length != 0) {
       return
-   } 
+   }
    else {
       document.write('ok')
    }
