3be0566e4ffedabb3fd1e1f3989e159633702ed6
Gustavo de Vito <gustavodevito38@gmail.com>
2021-04-29 23:18:36 -0300

    update api

--- src/static/js/api.js
+++ src/static/js/api.js
@@ -16,28 +16,39 @@ async function videoAll() {
         .catch((err) => {console.log(err);})
 }
 // videoAll()
-//IKzlry6Gt7Y
 
 async function videoSearch(id) {
     await axios.get('https://www.googleapis.com/youtube/v3/videos?key=' + apiKeyYoutube + '&part=snippet&id=' + id)
         .then((response) => {
-            console.log(response); //all data
-            console.log(response.data.items[0].snippet.title);
-            console.log(response.data.items[0].snippet.description);
-            //console.log(response.data.items[0].snippet.publishedAt);
-            let publ = response.data.items[0].snippet.publishedAt.substring(0, response.data.items[0].snippet.publishedAt.indexOf('T')).split('-')
-            console.log(`${publ[2]}/${publ[1]}/${publ[0]}`);
-            console.log(response.data.items[0].snippet.thumbnails.high.url);
-            //src in youtube https://www.youtube.com/embed/ + id in funtion
+            // console.log(response);
+
+            let description = response.data.items[0].snippet.description.split("\n")
+            let moreInformations = description[3].split('|')
+            let technicalTeam = response.data.items[0].snippet.description.substring(response.data.items[0].snippet.description.indexOf('EQUIPE TÉCNICA')).split('\n')
+
+            const movieInformations = {
+                title: response.data.items[0].snippet.title,
+                description: description[1],
+                duration: moreInformations[0].trim(),
+                type: moreInformations[1].trim(),
+                year: moreInformations[2].trim(),
+                technicalTeam: technicalTeam,
+            }
+
+            return JSON.parse(movieInformations)
         })
-        .catch((err) => {console.log(err);})
+        .catch((error) => {
+            return error
+        })
 }
-// videoSearch('IKzlry6Gt7Y')
+
+let x = videoSearch('ke8X3SE0XE8')
+console.log(x);
 
 async function sendEmail() {
-    await axios.post('https://api.staticforms.xyz/submit' + $("#staticform").serialize())
-    .then((response) => {
-            console.log(response);
-    })
-    .catch((err) => {console.log(err);})
+    await axios.post('https://api.staticforms.xyz/submit' + $(".formulario").serialize())
+        .then((response) => {
+                console.log(response);
+        })
+        .catch((err) => {console.log(err);})
 }
--- src/static/js/app.js
+++ src/static/js/app.js
@@ -1,11 +1,26 @@
 //NEBL4v7kTC4
 //H1Mw11tzusY
-movie = (link) => {
-    document.getElementById('youtube').innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${link}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+movie = (id) => {
+    let informations = videoSearch(id)
+    console.log(informations);
+    $('.wrapper').append(`<iframe src="https://www.youtube.com/embed/${id}" class="embed" title="Filme" allowfullscreen="true"></iframe>`)
+    $('.titulo').append(`<h2>${info.data.items[0].snippet.title}</h2>`)
+    $('.sinopse').append(`<h2>&nbsp;&nbsp;&nbsp;${info.data.items[0].snippet.description}</h2>`)
+    $('.dyg').append(`
+        <h2 class="duration">4min43s</h2>
+        <h2 class="year">${data.items[0].snippet.publishedAt.substring(0, response.data.items[0].snippet.publishedAt.indexOf('-'))}</h2>
+        <h2 class="genre">Terror</h2>
+    `)
+    $('.producao').append(`
+        <h2>Direção: Daniel Pires</h2>
+        <h2>Roteiro: Daniel Pires</h2> 
+        <h2>Equipe técnica: Universo Lenda</h2>
+    `)
 }
-// addVideo('')
+// movie('H1Mw11tzusY')
 
-$("#staticform").submit((event) => {
+//form 
+$(".formulario").submit((event) => {
     event.preventDefault();
     sendEmail()
 });
